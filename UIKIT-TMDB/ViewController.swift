c947f57683dd55bc1ef59f4cd84413dba08943fb
Marina De Pazzi <pazzi.dev@gmail.com>
2021-06-30 17:42:48 -0300

    chore: creating base project

---
+++ UIKIT-TMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKIT-TMDB
+//
+//  Created by Marina De Pazzi on 30/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
